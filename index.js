2e98f10a9130017e0aa216014162ee91393842e1
Jaydip Tank <95556824+Jaydip04@users.noreply.github.com>
2024-10-01 15:45:04 +0530

    update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("This is fetchData Api form link");
+  res.send("THis is update api");
 });
 
 app.get("/api/fetchData/", (req, res) => {
@@ -35,34 +35,27 @@ app.get("/api/fetchData/", (req, res) => {
     let responseData = {};
 
     // Determine the site and prepare the response data accordingly
-    if (
-        siteName.includes("YouTube") ||
-        url.includes("youtube.com") ||
-        url.includes("youtu.be")
-      ) {
-        responseData = {
-          message: "This video is from YouTube",
-          title: meta.title || "No title found",
-          videoUrl: meta.video.url || "No video URL found",
-        };
-      }  else if (
-        siteName.includes("Instagram") ||
-        url.includes("instagram.com")
-      ) {
+    if (siteName.includes("YouTube")) {
+      responseData = {
+        message: "This video is from YouTube",
+        title: meta.title || "No title found",
+        videoUrl: meta.video.url || "No video URL found",
+      };
+    } else if (siteName.includes("Instagram")) {
       responseData = {
         message: "This video is from Instagram",
         title: meta.title || "No title found",
         videoUrl: meta.url || "No video URL found",
         // likes: meta.site_name || "Likes information not available",
       };
-    } else if (siteName.includes("Facebook") || url.includes("facebook.com")) {
+    } else if (siteName.includes("Facebook")) {
       responseData = {
         message: "This video is from Facebook",
         title: meta.title || "No title found",
         videoUrl: meta.video.url || "No video URL found",
         // likes: meta.likes || "Likes information not available",
       };
-    } else if (siteName.includes("TikTok") || url.includes("tiktok.com")) {
+    } else if (siteName.includes("TikTok")) {
       responseData = {
         message: "This video is from TikTok",
         title: meta.title || "No title found",
