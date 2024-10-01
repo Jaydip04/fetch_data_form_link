04d1f346058d772a6136a6736b04067aa73382df
Jaydip Tank <95556824+Jaydip04@users.noreply.github.com>
2024-10-01 13:59:15 +0530

    update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get("/api/fetchData/", (req, res) => {
     let responseData = {};
 
     // Determine the site and prepare the response data accordingly
-    if (siteName.includes("YouTube")) {
+    if (meta.title == "YouTube") {
       responseData = {
         message: "This video is from YouTube",
         title: meta.title || "No title found",
